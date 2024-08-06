1f04a85113b1271f1d07f485a0e245412a750c2b
Dharunya S S <151717489+Dharunya-2003@users.noreply.github.com>
2024-08-06 18:14:46 +0530

    Add files via upload

---
+++ mark.php
@@ -0,0 +1,116 @@
+<?php
+
+require_once ('process/dbh.php');
+$id = (isset($_GET['id']) ? $_GET['id'] : '');
+$pid = (isset($_GET['pid']) ? $_GET['pid'] : '');
+
+
+echo $sql;
+$conn = mysqli_connect('localhost', 'root', '', 'ems');
+$result = mysqli_query($conn, $sql);
+if(isset($_POST['update']))
+{
+
+  $eid = mysqli_real_escape_string($conn, $_POST['eid']);
+  $pid = mysqli_real_escape_string($conn, $_POST['pid']);
+  
+
+  
+  $points = mysqli_real_escape_string($conn, $_POST['points']);
+  $base = mysqli_real_escape_string($conn, $_POST['base']);
+  $bonus = mysqli_real_escape_string($conn, $_POST['bonus']);
+  $total = mysqli_real_escape_string($conn, $_POST['total']);
+
+  $upSalary = $base + ($upBonus*$base)/100; 
+  echo "$upBonus";
+  echo "string";
+  echo "$upSalary";
+ 
+ 
+
+ $result = mysqli_query($conn, "UPDATE `rank` SET `points`='$upPoint' WHERE eid=$eid");
+ $result = mysqli_query($conn, "UPDATE `salary` SET `bonus`='$upBonus' ,`total`='$upSalary' WHERE id=$eid");
+
+
+
+
+ echo ("<SCRIPT LANGUAGE='JavaScript'>
+   
+    window.location.href='assignproject.php  ';
+    </SCRIPT>");
+
+  
+}
+?>
+
+
+
+
+<?php
+  $id = (isset($_GET['id']) ? $_GET['id'] : '');
+  $pid = (isset($_GET['pid']) ? $_GET['pid'] : '');
+  $sql1 = "SELECT pid, project.eid, project.pname, project.duedate, project.subdate, project.mark, rank.points, employee.firstName, employee.lastName, salary.base, salary.bonus, salary.total FROM `project` , `rank` ,`employee`, `salary`  WHERE project.eid = $id AND project.pid = $pid AND project.eid = rank.eid AND salary.id = project.eid AND employee.id = project.eid AND employee.id = rank.eid";
+  $result1 = mysqli_query($conn, $sql1);
+  if($result1){
+  while($res = mysqli_fetch_assoc($result1)){
+  $pname = $res['pname'];
+  $duedate = $res['duedate'];
+  $subdate = $res['subdate'];
+  $firstName = $res['firstName'];
+  $lastName = $res['lastName'];
+  $mark = $res['mark'];
+  $points = $res['points'];
+  $base = $res['base'];
+  $bonus = $res['bonus'];
+  $total = $res['total'];
+  
+}
+}
+
+?>
+
+<html>
+<head>
+  <title>Project Mark | Employee Management System</title>
+  <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
+</head>
+<body>
+  <header>
+    <nav>
+      <h1>EMS</h1>
+      <ul id="navli">
+        <li><a class="homeblack" href="aloginwel.php">HOME</a></li>
+        <li><a class="homeblack" href="addemp.php">Add Employee</a></li>
+        <li><a class="homeblack" href="viewemp.php">View Employee</a></li>
+        <li><a class="homeblack" href="assign.php">Assign Project</a></li>
+        <li><a class="homered" href="assignproject.php">Project Status</a></li>
+        <li><a class="homeblack" href="salaryemp.php">Salary Table</a></li>
+        <li><a class="homeblack" href="empleave.php">Employee Leave</a></li>
+        <li><a class="homeblack" href="alogin.html">Log Out</a></li>
+      </ul>
+    </nav>
+  </header>
+  
+ 
+                    </form>
+                    <br>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
